ac88c74204c569ef62a6391b91282986d58372c2
patrickpynadath <patrickpynadath2022@u.northwestern.edu>
2023-04-26 16:18:12 -0500

    added more structure for front-end + algo development workspace

--- SimulationEngine/__init__.py
+++ SimulationEngine/__init__.py
@@ -1,1 +1,5 @@
-from SimulationEngine.Chunker.frequency_chunker import *
+from ActionAlgorithms import Actor
+from Chunker import FreqChunker, Chunker
+from Portfolio import BasicPortfolio
+from DataPipeline import DataPipe
+from SignalGenerator import BaseGenerator
